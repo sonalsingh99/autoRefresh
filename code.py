e05a41e22b50686bcee52a58d3131e39a014c000
sonal singh <sonal.singh221999@gmail.com>
2020-09-14 11:43:59 +0530

    initial commit

---
+++ code.py
@@ -0,0 +1,37 @@
+import pynput
+from pynput.keyboard import Key,Controller
+import time
+def mainProgram(refresh_interval,stopper):
+        keyboard=Controller()
+        for _ in range(0,stopper):
+            time.sleep(refresh_interval)
+            keyboard.press(Key.cmd_l)
+            keyboard.press("d")
+            keyboard.release("d")
+            keyboard.release(Key.cmd_l)
+            keyboard.press(Key.f5)
+            keyboard.release(Key.f5)
+            keyboard.press(Key.cmd_l)
+            keyboard.press("d")
+            keyboard.release("d")
+            keyboard.release(Key.cmd_l)
+           
+            
+        return()
+        
+if __name__=='__main__': 
+    refresh_interval=int(input("Enter the time interval for refreshing in seconds : "))
+    choose_run=input("Click 'A' to give number of times you want to refresh/ Click any key for default setting.")
+    
+    if choose_run=="A" or choose_run=="a":
+        stopper= int(input("Enter for how many times do you want to refresh: "))
+        mainProgram(refresh_interval,stopper)
+    else:      
+        for _ in range(0,4):
+                mainProgram(refresh_interval,5)
+               
+                Quit=input("Select 'Y' to quit the process/and any key to elsewise: ")
+                if Quit=="Y" or Quit =="y":
+                    break
+                else:
+                    mainProgram(refresh_interval,5)
